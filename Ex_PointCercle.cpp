74c024ff5e885c89893c97cafc9abf0b4b5d6270
HIND OUAÂOUCH <waaouchhind@gmail.com>
2024-10-17 23:54:34 +0100

    Ex_PointCercle.cpp

--- Ex_PointCercle.cpp
+++ Ex_PointCercle.cpp
@@ -3,22 +3,22 @@
 #include <iostream>
 
 int main() {
-    // Création de deux points
-    Point p1(3.0, 4.0);
-    Point p2(6.0, 8.0);
+    // Création dynamique de deux points
+    Point* p1 = new Point(3.0, 4.0);
+    Point* p2 = new Point(6.0, 8.0);
 
     // Affichage des points
     std::cout << "Point 1:" << std::endl;
-    p1.afficher();
+    p1->afficher();
     std::cout << "Point 2:" << std::endl;
-    p2.afficher();
+    p2->afficher();
 
     // Calcul et affichage de la distance entre les deux points
-    std::cout << "Distance entre Point 1 et Point 2: " << p1.distance(p2) << std::endl;
+    std::cout << "Distance entre Point 1 et Point 2: " << p1->distance(*p2) << std::endl;
 
-    // Création de deux cercles
-    Cercle* c1 = new Cercle(1, 5.0, p1);
-    Cercle* c2 = new Cercle(2, 5.0, p2);
+    // Création dynamique de deux cercles avec les points créés dynamiquement
+    Cercle* c1 = new Cercle(1, 5.0, *p1);
+    Cercle* c2 = new Cercle(2, 5.0, *p2);
 
     // Affichage des cercles
     std::cout << "\nCercle 1:" << std::endl;
@@ -42,21 +42,23 @@ int main() {
     std::cout << "Perimetre de Cercle 2: " << c2->Perimetre() << std::endl;
 
     // Test si un point appartient au cercle
-    if (c1->apartient(p2)) {
+    if (c1->apartient(*p2)) {
         std::cout << "Le point 2 appartient au Cercle 1." << std::endl;
     }
     else {
-        std::cout << "Le point 2 n'appartient pas au Cercle 1." << std::endl;
+        std::cout << "Le point 2 n appartient pas au Cercle 1." << std::endl;
     }
 
     // Modifier le rayon du premier cercle
     c1->Modifier(7.0);
-    std::cout << "\nAprès modification du rayon de Cercle 1:" << std::endl;
+    std::cout << "\nApres modification du rayon de Cercle 1:" << std::endl;
     c1->afficher();
 
-    // Libérer la mémoire allouée pour les cercles
+    // Libérer la mémoire allouée pour les cercles et les points
     delete c1;
     delete c2;
+    delete p1;
+    delete p2;
 
     return 0;
 }
