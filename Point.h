ff9c64263d309188bdc7cb9d1b4640bf856b8c6a
HIND OUAÂOUCH <waaouchhind@gmail.com>
2024-11-03 23:38:54 +0100

    Point.h

--- Point.h
+++ Point.h
@@ -1,18 +1,19 @@
 #pragma once
-
 class Point
 {
 private:
 	double x;
 	double y;
-
+	Point();
+	Point(double x = 0.0, double y = 0.0);
+	Point(const Point& c);
 public:
-	Point(double x = 0.0, double y = 0.0);//constructeur avc et sans para
+	static Point* fabriquer(double x = 0.0, double y = 0.0);
+	void translate(double x, double y);
 	void afficher() const;
 	double distance(const Point& p) const;
-	void translater(double dx, double dy);
-	bool operator==(const Point&) const;
+	bool operator==(const Point& p) const;
 	~Point();
-
 };
+
 
